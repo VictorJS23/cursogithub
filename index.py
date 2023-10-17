aa15b6a84a5be7abebb201a316cdb33a20d1628a
VictorJS23 <vb329708@gmail.com>
2023-10-16 20:30:27 -0600

    commit

--- index.py
+++
@@ -1,3 +0,0 @@
-print("Hola mundo desde la terminal con vim instalado en linux")
-
-
--- main.js
+++ main.js
@@ -1,28 +1,11 @@
-console.log("Hola Mundo desde JavaScript en linux")
+const boton = document.getElementById('btn')
 
-const factorial = ((n) => {
-    if(n === 0 || n === 1){
-        return 1
-    } else {
-        return n * factorial(n - 1)
-    }
+boton.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const nombre = document.getElementById('input').value
+
+    const parrafo = document.getElementById('nombreEmpresa')
+
+    parrafo.textContent = nombre
 })
-
-let result = factorial(4)
-
-console.log(result)
-
-const array = [1, 2, 3, 4, 5, 6]
-
-console.log(array)
-
-console.log('---------------------------------------')
-
-const arrayFilter = array.filter((n) => (n % 2 === 0)).map(function(n) {
-    return factorial(n)
-})
-
-// esto devuelve los elementos que son pares del arreglo y luego se mapean
-// para calcular el factorial de cada uno de ellos
-
-console.log(arrayFilter)
